0b3dc2de4c9460209759d9e392723bd3b5d8fdd4
jonathanBoada29 <jonathanboada71@gmail.com>
2024-05-27 20:27:47 -0500

    Se añadierpn los productos a la vista del carrito

--- src/main/java/com/shoe/platform/controller/HomeController.java
+++ src/main/java/com/shoe/platform/controller/HomeController.java
@@ -53,7 +53,7 @@ public class HomeController {
 	}
 	
 	@PostMapping("/cart")
-	public String addCard(@RequestParam Integer id,@RequestParam Integer cantidad) {
+	public String addCard(@RequestParam Integer id,@RequestParam Integer cantidad, Model model) {
 		
 		DetalleOrden detalleOrden = new DetalleOrden();
 		Producto producto = new Producto();
@@ -62,6 +62,19 @@ public class HomeController {
 		Optional<Producto> optionalProducto = productoService.get(id);	
 		log.info("Producto añadido: {}",optionalProducto.get());
 		log.info("Cantidad: {}", cantidad);
+		
+		producto=optionalProducto.get();
+		detalleOrden.setCantidad(cantidad);
+		detalleOrden.setPrecio(producto.getPrecio());
+		detalleOrden.setNombre(producto.getNombre());
+		detalleOrden.setTotal(producto.getPrecio()*cantidad);
+		detalleOrden.setProducto(producto);
+		
+		detalles.add(detalleOrden);
+		
+		sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
 		
 		return "usuario/carrito";
 	}
