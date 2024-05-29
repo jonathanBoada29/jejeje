6f25dd380e2bd588ad047d042778fa70d0f73d51
jonathanBoada29 <jonathanboada71@gmail.com>
2024-05-28 21:36:14 -0500

    Validacion para doble producto y validar el carrito desde la cabecera del template_usuario menu opcion carrito

--- src/main/java/com/shoe/platform/controller/HomeController.java
+++ src/main/java/com/shoe/platform/controller/HomeController.java
@@ -70,7 +70,12 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio()*cantidad);
 		detalleOrden.setProducto(producto);
 		
-		detalles.add(detalleOrden);
+		//validar que el producto no se añada mas de una vez
+		Integer idProducto=producto.getId();
+		boolean ingresado=detalles.stream().anyMatch(p -> p.getProducto().getId()==idProducto);
+		if (!ingresado) {
+			detalles.add(detalleOrden);
+		}
 		
 		sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 		orden.setTotal(sumaTotal);
@@ -103,5 +108,12 @@ public class HomeController {
 			model.addAttribute("orden", orden);
 
 			return "usuario/carrito";
+		}
+		
+		@GetMapping("/getCart")
+		public String getCart(Model model) {
+			model.addAttribute("cart", detalles);
+			model.addAttribute("orden", orden);
+			return "/usuario/carrito";
 		}
 }
